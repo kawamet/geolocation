43548b3dfa2a5f58a1f0c9d4e3a4d1729d1ed3d7
Karolina Wanat <carolinawanat@gmail.com>
2020-04-05 21:48:50 +0100

    carusel added

--- src/main/java/uk/kawamet/geolocation/DownloadFile.java
+++ src/main/java/uk/kawamet/geolocation/DownloadFile.java
@@ -5,11 +5,12 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DownloadFile {
 
-    public void downloadFile() throws IOException {
+    public void downloadFile() throws MalformedURLException {
 
         URL url = new URL("https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv");
 
@@ -18,6 +19,10 @@ public class DownloadFile {
         int connectionTimeout = 10 * 1000; // 10 sec
         int readTimeout = 300 * 1000; // 3 min
 
-        FileUtils.copyURLToFile(url, file, connectionTimeout, readTimeout);
+        try {
+            FileUtils.copyURLToFile(url, file, connectionTimeout, readTimeout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
