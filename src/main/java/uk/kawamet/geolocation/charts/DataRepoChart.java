d95a079cc6da55190586e00d3753eea7e4979379
Karolina Wanat <carolinawanat@gmail.com>
2020-07-02 20:13:28 +0100

    bar chart - limit to 10 highest records

--- src/main/java/uk/kawamet/geolocation/charts/DataRepoChart.java
+++ src/main/java/uk/kawamet/geolocation/charts/DataRepoChart.java
@@ -2,8 +2,10 @@ package uk.kawamet.geolocation.charts;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 public class DataRepoChart {
@@ -17,7 +19,15 @@ public class DataRepoChart {
         surveyMap.put(country, cases);
     }
 
+    //top 10
     public Map<String, Integer> getSurveyMap() {
-        return surveyMap;
+        HashMap<String, Integer> collect = surveyMap
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue((a, b) -> b - a)).limit(10).collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                (oldValue, newValue) -> oldValue, HashMap::new));
+        return collect;
     }
 }
